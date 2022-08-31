c69f16f7d5d3c980a25f2f4df328a4d6f64f36e8
fujisii <51264835+fujisii@users.noreply.github.com>
2022-08-31 12:43:38 +0900

    ドメイン駆動設計入門 p32

--- app/ValueObjects/name.php
+++ app/ValueObjects/name.php
@@ -6,7 +6,7 @@ class Name
 
     public function __construct(string $value)
     {
-        if ($value === '') throw new InvalidArgumentException('$value is Null');
+        if ($value === '') throw new InvalidArgumentException('1文字以上である必要があります。');
         if (!preg_match('/^[a-zA-Z]+$/', $value)) throw new InvalidArgumentException('許可されていない文字が使われています。');
 
         $this->value = $value;
