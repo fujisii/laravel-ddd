befa1cca9c3d6f3534765ae8b67fc13ae700787e
fujisii <51264835+fujisii@users.noreply.github.com>
2022-09-01 12:34:55 +0900

    ドメイン駆動設計入門 p38

---
+++ app/ValueObjects/ModelNumber.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+class ModelNumber
+{
+    private string $productCode;
+    private string $branch;
+    private string $lot;
+
+    public function __construct(string $productCode, string $branch, string $lot)
+    {
+        if ($productCode === '') throw new InvalidArgumentException('1文字以上である必要があります。');
+        if ($branch === '') throw new InvalidArgumentException('1文字以上である必要があります。');
+        if ($lot === '') throw new InvalidArgumentException('1文字以上である必要があります。');
+
+        $this->productCode = $productCode;
+        $this->branch = $branch;
+        $this->lot = $lot;
+    }
+
+    public function ToString()
+    {
+        return new $this->productCode . '-' . $this->branch . '-' . $this->lot;
+    }
+}
