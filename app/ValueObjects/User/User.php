a35f4668f690f719435b75d60b585f7f4305b2e3
fujisii <51264835+fujisii@users.noreply.github.com>
2022-09-01 12:47:42 +0900

    ドメイン駆動設計入門 p43

---
+++ app/ValueObjects/User/User.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+require_once 'UserId.php';
+require_once 'UserName.php';
+
+class User
+{
+    private UserId $Id;
+    private UserName $Name;
+
+    public function __construct()
+    {
+    }
+
+    public function getId()
+    {
+        return $this->Id;
+    }
+    public function setId(UserId $Id)
+    {
+        $this->Id = $Id;
+    }
+
+    public function getName()
+    {
+        return $this->Name;
+    }
+
+    public function setName(UserName $Name)
+    {
+        $this->Name = $Name;
+    }
+
+    public function CreateUser(UserName $Name)
+    {
+        $user = new User();
+        $user->setId($Name);
+    }
+}
